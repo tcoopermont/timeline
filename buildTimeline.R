290a5f3cccde5ebf81ecfc951e7b127e02e46c74
Tom Cooper <tomcooper@tec8.net>
2017-07-09 12:34:23 -0700

    broke filter ops to function

--- buildTimeline.R
+++ buildTimeline.R
@@ -2,12 +2,21 @@ library(dplyr)
 library(ggplot2)
 
 buildTimeline = function(quakeData,minYear,maxYear,countryList) {
-  quakesFiltered <- filter(quakeData,YEAR >= minYear,
-                   YEAR <= maxYear,
-                    COUNTRY %in% countryList) %>%
-                 mutate(COUNTRY = factor(COUNTRY))
-  ggplot(quakesFiltered,aes(x = YEAR,y = COUNTRY,xmin = minYear,xmax = maxYear,name = LOCATION_NAME)) +
+  quakesFiltered <- filterQuakes( quakeData,minYear,maxYear,countryList)
+  ggplot(quakesFiltered,aes(x = YEAR,y = COUNTRY,
+         xmin = minYear,xmax = maxYear,name = LOCATION_NAME,
+          #size = EQ_PRIMARY 
+         )) +
   geom_timeline()
 
    
 }
+filterQuakes = function(quakeData,minYear,maxYear,countryList) {
+  filter(quakeData,YEAR >= minYear,
+                   YEAR <= maxYear,
+                    COUNTRY %in% countryList) %>%
+                 mutate(COUNTRY = factor(COUNTRY))
+}
+
+
+#EQ_PRIMARY,EQ_MAG_MW,EQ_MAG_MS,EQ_MAG_MB,EQ_MAG_ML,EQ_MAG_MFA,EQ_MAG_UNK
