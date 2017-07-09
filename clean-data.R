11fa0d2e6863ad5bcb62e4e40ae8384fc4aa42d9
Tom Cooper <tomcooper@tec8.net>
2017-07-09 11:05:02 -0700

    added clean functions

---
+++ clean-data.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(tools)
+eq_clean_data <- function(adf) {
+  mutate(adf, LATITUDE =  as.numeric(LATITUDE),
+              LONGITUDE = as.numeric(LONGITUDE),
+              date = as.Date(paste(YEAR,MONTH,DAY,sep="-"),"%Y-%m-%d") 
+        )
+}
+
+eq_location_clean <- function(adf) {
+  mutate(adf,LOCATION_NAME = toTitleCase(tolower(
+             sub("^[^:]+:\\s+" ,"",LOCATION_NAME,perl = TRUE))))
+
+}
+#eqd1 <- mutate(eqd, LATITUDE =  as.numeric(LATITUDE),LONGITUDE = as.numeric(LONGITUDE))
+# mutate(eqd,date = as.Date(paste(YEAR,MONTH,DAY,sep="-"),"%Y-%m-%d"))
+
+# toTitleCase(tolower(tail(eqd$LOCATION_NAME)))
+#sub("^[^:]+:\\s+" ,"",tail(eqd$LOCATION_NAME,500),perl = TRUE)
+
+
