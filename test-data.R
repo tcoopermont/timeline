56ba251ec1418729648e3d3560fe9e0520ff2470
Tom Cooper <tomcooper@tec8.net>
2017-07-09 13:49:00 -0700

    added file w/ mock-data

---
+++ test-data.R
@@ -0,0 +1,8 @@
+
+skdf <- data.frame(x = c(1988,1989,1990,1995,2000,2005,2011,2014,2015),
+                   y=c(1,1,1,2,1,1,2,2,1),
+                   rict = c(5,6,8,2,4,3,5,6,4),
+                   name = c("Fox Island","Southereastern","Aleutian Islands",
+                            "Rat Islands","Fairbanks","Big Chain Mts.","Black Forest",
+                             "Yellowstone","Mt Rainier")
+                   )
